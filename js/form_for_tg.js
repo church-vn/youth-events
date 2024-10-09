0f09ac360715789ec14a7799490021c1857d1323
PandaDevelopmentCompany <pandadesignstudio@yandex.ru>
2024-10-09 17:13:25 +0300

    download_09_10_2024_7

---
+++ js/form_for_tg.js
@@ -0,0 +1,38 @@
+const token = '7738919764:AAECEuRqdJXpYrEaFIlbd72-xjhlYR_9Ec4';
+const chatId = '746586393';
+const URL_API = 'https://api.telegram.org/';
+
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+
+sendButton.addEventListener('click', (event) => {
+event.preventDefault();
+const message = 'Имя: ' + sendName.value + '\nНомер телефона: ' + sendTel.value + '\nТема сообщения: ' + messageInput.value + '\nКодовое слово: ' + messageInputpass.value;
+if (message) {
+axios.post(`${URL_API}bot${token}/sendMessage`, {
+chat_id: chatId,
+text: message,
+})
+.then(response => console.log(response), alert('Ваша заявка на уведомления зарегистрирована!✅ \nСпасибо! \nПриятно времяпрепровождения!💚'))
+.catch(error => console.error(error));
+}
+});
+
+
+
+
+const messageInput1 = document.getElementById('messageInput1');
+const sendButton1 = document.getElementById('sendButton1');
+
+sendButton1.addEventListener('click', (event) => {
+event.preventDefault();
+const message = 'Name : ' + sendName.value + '\nPhone number: ' + " " + sendTel.value + '\nApplication comment: ' + messageInput.value;
+if (message) {
+axios.post(`${URL_API}bot${token}/sendMessage`, {
+chat_id: chatId,
+text: message,
+})
+.then(response => console.log(response), alert('Your message has been successfully sent!✅ \nWe will definitely contact you! \nThank you!💚'))
+.catch(error => console.error(error));
+}
+});
